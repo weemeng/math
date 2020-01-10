754e6aaed25cdf70dbfcc55cf77587f08531e90b
weemeng <weemeng@users.noreply.github.com>
2020-01-10 17:14:21 +0800

    edited the subtract function

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const math = {
   add: (...numbers) => {
-    return numbers.reduce((sum, val) => sum + val,0);
+    return numbers.reduce((sum, val) => sum + val, 0);
   },
   randfloor: a => {
     return Math.floor(Math.random() * a);
+  },
+  subtract: function(initVal, ...numbers) {
+    return initVal - this.add(...numbers);
+    // return initVal - this.add(numbers)
   }
 };
 module.exports = math;
